5d1c1771057f14a6b447c4a5c9c4529f23684c7f
chiman <rlaclghks123@naver.com>
2022-12-20 15:52:49 +0900

    feat: Carousel 컴포넌트 추가

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,99 @@
+import React, { Children, useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
+const CarouselContainer = styled.div`
+  width: ${({ itemWidth }) => `${itemWidth}px`};
+  position: relative;
+  background-position: center;
+  background-size: cover;
+  overflow: hidden;
+  border-radius: 10px;
+`;
+
+const NavigationWrapper = styled.div`
+  width: ${({ itemWidth }) => `${itemWidth}px`};
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  background-color: transparent;
+  height: fit-content;
+  z-index: 5;
+`;
+
+const CarouselItemsWrapper = styled.div`
+  width: ${({ translateXAmountLimit }) => `${translateXAmountLimit}px`};
+  height: 100%;
+  display: flex;
+  transform: translateX(${({ translateXAmount }) => `${translateXAmount}px`});
+  transition: transform 0.7s ease-in-out;
+`;
+
+const Button = styled.button`
+  background-color: inherit;
+  border: none;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+function Carousel({
+  autoPlay = true,
+  autoPlayDuration = 2000,
+  children,
+  itemWidth = 400,
+  ...others
+}) {
+  const [translateXAmount, setTranslateXAmount] = useState(0);
+  const itemCount = Children.toArray(children).length;
+  const translateXAmountLimit = itemCount * itemWidth;
+
+  const onClickLeft = () => {
+    const newTranslateXAmount = (translateXAmount + itemWidth) % translateXAmountLimit;
+    setTranslateXAmount(
+      (newTranslateXAmount > 0 ? -translateXAmountLimit : 0) + newTranslateXAmount
+    );
+  };
+
+  const onClickRight = useCallback(() => {
+    setTranslateXAmount((translateXAmount - itemWidth) % translateXAmountLimit);
+  }, [translateXAmount, itemWidth, translateXAmountLimit]);
+
+  useEffect(() => {
+    setTranslateXAmount(0);
+  }, [itemWidth]);
+
+  useEffect(() => {
+    let timer = autoPlay ? setTimeout(onClickRight, autoPlayDuration) : 0;
+    return () => {
+      if (timer !== 0) clearTimeout(timer);
+    };
+  }, [translateXAmount, onClickRight, autoPlay, autoPlayDuration]);
+
+  return (
+    <CarouselContainer itemWidth={itemWidth} {...others}>
+      <NavigationWrapper itemWidth={itemWidth}>
+        <Button onClick={onClickLeft}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </Button>
+
+        <Button onClick={onClickRight}>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </Button>
+      </NavigationWrapper>
+
+      <CarouselItemsWrapper
+        translateXAmountLimit={translateXAmountLimit}
+        translateXAmount={translateXAmount}
+      >
+        {children}
+      </CarouselItemsWrapper>
+    </CarouselContainer>
+  );
+}
+
+export default Carousel;
--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,14 +1,27 @@
 import styled from 'styled-components';
+import Carousel from '../components/Carousel';
+import DoubleBox from '../components/DoubleBox';
 
-const Wrapper = styled.div`
-  margin-top: 50px;
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  background: ${(props) => props.theme.bgColor};
+const ItemImg = styled.div`
+  background-position: center;
+  background-size: cover;
+  background-image: url(${({ imgSrc }) => imgSrc});
+  width: 90vw;
+  height: 35vw;
 `;
 
 function Projects() {
-  return <Wrapper>hi</Wrapper>;
+  return (
+    <DoubleBox>
+      <Carousel>
+        {['images/고래.png', 'images/다람쥐.png', 'images/독수리.png', 'images/새.png'].map(
+          (imgSrc, i) => {
+            return <ItemImg imgSrc={imgSrc} key={i} />;
+          }
+        )}
+      </Carousel>
+      <div>2</div>
+    </DoubleBox>
+  );
 }
 export default Projects;
