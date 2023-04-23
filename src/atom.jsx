3b7f18794338fec98ce29fb47936ad3a992a3925
chiman <rlaclghks123@naver.com>
2023-04-23 19:59:31 +0900

    Refactor: 불필요한 부분 제거

--- src/atom.jsx
+++ src/atom.jsx
@@ -150,10 +150,6 @@ export const projectContents = atom({
           contents:
             'Vanilla Js를 사용해서 NodeJS의 콘솔창에서 입력을 받아, 출력을 나타내며, Class를 사용하여 모듈화를 했으며, 유지보수 및 가독성을 위해 MVC패턴을 적용했습니다. 또한  프로젝트의 안정감을 더할수 있도록 jest를 활용하여 코드를 Test 했습니다.',
         },
-        {
-          title: '문제발생과 해결',
-          contents: '11',
-        },
 
         {
           title: 'Clean Code',
