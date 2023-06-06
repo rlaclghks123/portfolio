c3e00462a1d75194e849be8d144cece7220ed3c3
chiman <rlaclghks123@naver.com>
2023-06-07 00:17:29 +0900

    Refactor: atom 정보 수정

--- src/atom.jsx
+++ src/atom.jsx
@@ -48,23 +48,15 @@ export const projectContents = atom({
               detail: [
                 {
                   text: [
-                    '상태 관리: React Hook Form은 상태 관리를 대신 처리합니다. 기존의 상태 관리 라이브러리인 useState나 useContext 등을 사용하여 개별적으로 상태를 관리하는 대신, React Hook Form에서 제공하는 useForm Hook을 사용하여 폼 상태를 관리합니다. 이로 인해 코드가 간결해지고, 상태 관리에 대한 부담이 줄어듭니다.',
+                    '성능 최적화: React Hook Form은 input값이 변해도 리랜더링이 발생하지 않습니다. React-Hook-Form 사용시 불필요한 리렌더링을 줄여 성능을 향상시킵니다.',
                   ],
-                  img: 'https://rlaclghks123.github.io/portfolio/images/review-cock/notLoginHome.jpg',
+                  img: 'https://rlaclghks123.github.io/portfolio/images/review-cock/reactHookFormDiff.jpg',
                 },
                 {
                   text: [
-                    '폼 유효성 검사: React Hook Form은 내장된 유효성 검사 기능을 제공합니다. 개발자는 간단한 스키마 기반의 규칙을 사용하여 필드 유효성을 정의할 수 있습니다. 이를 통해 사용자 입력의 유효성을 쉽게 검사하고, 에러 메시지를 처리할 수 있습니다.',
-                  ],
-                },
-                {
-                  text: [
-                    '성능 최적화: React Hook Form은 성능 최적화를 고려하여 설계되었습니다. 사용자 입력이 변경될 때에만 필요한 부분만 다시 렌더링되도록 설계되어 있습니다. 이는 불필요한 리렌더링을 줄여 성능을 향상시킵니다.',
-                  ],
-                },
-                {
-                  text: [
-                    '커스텀 입력 컴포넌트: React Hook Form은 커스텀 입력 컴포넌트를 쉽게 작성할 수 있는 방법을 제공합니다. 이를 통해 재사용 가능한 입력 컴포넌트를 만들고, 폼 로직을 분리하여 코드의 재사용성을 높일 수 있습니다.',
+                    `상태 관리,유효성 검사,에러처리
+                    기존의 Form 방식은 상태관리, 유효성 검사, 에러처리를 직접 작성해야 하며 여러개의 useState를 사용하여 처리를 해야합니다.
+                    반면 react-hook-form 라이브러리를 사용하면 상태관리, 유효성 검사, 에러처리를  useForm Hook을 통해 쉽게 관리가 가능하며, 간결해지기 때문에 가독성, 유지보수 측면에서 더욱 좋습니다.`,
                   ],
                 },
               ],
@@ -74,18 +66,18 @@ export const projectContents = atom({
               detail: [
                 {
                   text: [
-                    '사용자 경험을 향상시키위해 브라우저에 저장하여 반복적인 로그인 작업을 줄여 시간과 노력을 절약해주기 위해 저장을 합니다.',
-                  ],
-                  img: 'https://rlaclghks123.github.io/portfolio/images/review-cock/notLoginHome.jpg',
-                },
-                {
-                  text: [
-                    '쿠키에 저장하는 이유는 브라우저를 껏다 켜도 쿠키에 저장되어 있으며, 해커가 이메일만으로는 어떻게 할 수 없다고 생각하여, 보안에 큰 문제도 없기 때문에 쿠키에 저장을 했습니다.',
+                    `아이디를 저장하는 이유 : 사용자 경험을 향상시키위해 브라우저에 저장하여 반복적인 로그인 작업을 줄여 시간과 노력을 절약해주기 위해서 입니다.`,
                   ],
                 },
                 {
                   text: [
-                    '물론 보안적인 부분에서 email만으로 해커가 직접적으로 해킹을 할 수 있는것은 아닙니다. 하지만 이메일 주소를 통해 "피싱" 공격이 가능합니다. 피싱은 이메일을 통해 사용자들을 속여 개인 정보나 로그인 자격 증명을 요구하는 가짜 웹 사이트로 유도하는 공격입니다.',
+                    '쿠키에 저장하는 이유 : 브라우저를 껏다 켜도 쿠키에 저장되어 있으며, 해커가 이메일만으로는 어떻게 할 수 없다고 생각하여 보안에 큰 문제가 없다고 생각하여 쿠키에 저장을 했습니다.',
+                  ],
+                },
+                {
+                  text: [
+                    `그럼 LocalStorage에 저장해도 되는데 왜 쿠키에 저장을 선택했는지? 
+                  물론 보안적인 부분에서 email만으로 해커가 직접적으로 해킹을 할 수 있는것은 아닙니다. 하지만 이메일 주소를 통해 "피싱" 공격이 가능합니다. 피싱은 이메일을 통해 사용자들을 속여 개인 정보나 로그인 자격 증명을 요구하는 가짜 웹 사이트로 유도하는 공격입니다.`,
                   ],
                 },
                 {
@@ -95,8 +87,41 @@ export const projectContents = atom({
                 },
               ],
             },
-            { title: '회의를 통해 내린 결론을 문서화 하지 않은 경우 발생하는 문제점' },
-            { title: 'Git Flow 방식을 사용하여 개발하면 좋은점' },
+            {
+              title: '큰 틀만 정하고 구체적인 부분에 대해 문서화 하지 않은 경우 발생하는 문제점',
+              detail: [
+                {
+                  text: [
+                    '기획자, 디자이너, 개발자 간에 회의를 통해 의논한 내용을 문서화하지 않고 머릿속으로만 기억하여 구현하는 데 어려움을 겪었습니다. 이로 인해 프로젝트를 진행하는데 있어서 혼란이 생겼습니다.',
+                  ],
+                },
+                {
+                  text: [
+                    '개별적으로 개발에 회의를 통해 바뀌는 부분을 작성해두지 않아, 대화를 진행한 두 사람을 제외하고는 수정 사항을 알지 못하고 프로젝트 전체를 이해하는데 어려움이 발생하였습니다.',
+                  ],
+                },
+                {
+                  text: [
+                    '문서화를 하지 않아 전반적인 진행 상황과 업무 분배, 일정 계획 등을 파악하기가 어려웠고, 팀원들 간의 협업과 일정 관리에 어려움을 겪었습니다.',
+                  ],
+                },
+              ],
+            },
+            {
+              title: 'Git Flow 방식에 대해 배웠습니다.',
+              detail: [
+                {
+                  text: [
+                    '안정성과 신뢰성: main 브랜치를 항상 안정적인 코드로 유지하고, 기능 개발은 feature 브랜치에서 진행하며, 완료되면 dev 브랜치로 병합후 문제가 없으면 main브랜치로 병합하며  코드의 안정성과 신뢰성을 유지할 수 있습니다.',
+                  ],
+                },
+                {
+                  text: [
+                    '협업의 원활성: Git flow 방식을 통해  모두 독립적으로 작업을 진행했고, 브랜치를 통한 작업 분리로 인해 충돌이 최소화 됐습니다. 또한, 코드 리뷰와 병합 작업을 통해 팀원들과의 협업이 원활하게 진행되었습니다.',
+                  ],
+                },
+              ],
+            },
           ],
         },
         {
@@ -105,6 +130,31 @@ export const projectContents = atom({
             {
               title:
                 '브라우저 창이 꺼지거나 페이지가 새로고침 되는 경우 Access Token이 사라져 에러 발생',
+              detail: [
+                {
+                  text: [
+                    `새로고침시 로컬에 저장해뒀던 AccessToken이 없어지는 문제가 발생
+문제 발생 이유: axiosInstance를 통해 HTTP 요청을 보낼때 마다 Header에 accessToken을 보내도록 설정을 해줬습니다.
+ 하지만 새로고침시 리랜더링이 발생하고, 이때 로컬에 저장 되어있던 Access-Token이 초기화되어 사라지는것입니다.`,
+                  ],
+                },
+                {
+                  text: [
+                    `문제해결
+                  App컴포넌트에 아래의 사진에 있는 코드를 실행시켜주면 됩니다.
+                  `,
+                  ],
+                  img: 'https://rlaclghks123.github.io/portfolio/images/review-cock/persistentLogin.jpg',
+                },
+
+                {
+                  text: [
+                    `작동 이유
+                  새로고침시 header에 저장된 값은 날라가지만, 쿠키에 Refresh-Token을 저장해뒀기 때문에 이것을 통해 Access-Token을 재발급 받아 로그인 유지가 가능합니다.
+                  `,
+                  ],
+                },
+              ],
             },
           ],
         },
@@ -200,7 +250,8 @@ export const projectContents = atom({
               img: 'https://rlaclghks123.github.io/portfolio/images/todo_list_img/problem1.jpg',
             },
             {
-              title: 'filter 함수 사용시 비교하는 요소의 데이터 타입이 달라 작동하지 않은 문제',
+              title:
+                'toDoList를 삭제하기 위해 filter 함수를 사용하여 클릭한 li와 toDoList에 있는 li들을 비교해 List를 삭제하려던 중 데이터 타입이 달라 작동하지 않은 문제 발생',
               img: 'https://rlaclghks123.github.io/portfolio/images/todo_list_img/problem2.jpg',
             },
           ],
